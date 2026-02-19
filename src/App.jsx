9504baf43c5118513b82ff89b891c5a6feba7547
Timuçin TOPCU <57364940+Teamuchin@users.noreply.github.com>
2026-02-19 21:18:26 +0300

    Register added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import './App.css'
 import Login from "./pages/Login.jsx"
 import Banner from "./pages/Banner.jsx"
 import Home from "./pages/Home.jsx"
+import Register from "./pages/Register.jsx"
 function App() {
   return (
     <>
-    <Home />
+    <Register />
     </>
   )
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,23 @@
+import styles from "./Register.module.css"
+export default function Register(){
+    function signUp(registerData){
+        
+    }
+    return(
+        <div className={styles.container}>
+            <div className={styles.labeldiv} >
+                <img className={styles.logolabel} src="../public/logo.svg" alt="websitelogo"></img>
+                <h1 className={styles.label}>Mangala</h1>
+            </div>
+            <form className={styles.formdiv} action={signUp}>
+                <input id="username" defaultValue="" type="text" name="username" placeholder="Username" className={styles.textinput} />
+                <input id="email" defaultValue="" type="email" name="email" placeholder="Email" className={styles.textinput} />
+                <input id="userpwd" defaultValue="" type="password" name="userpwd" placeholder="Password" className={styles.textinput}/>
+                <input id="confirmpwd" defaultValue="" type="password" name="confirmpwd" placeholder="Confirm Password" className={styles.textinput}/>
+                <input type="submit" value="Sign Up" className={styles.submitbtn}/>
+            </form>
+            <hr className={styles.horizline} data-content = "OR"></hr>
+            <button className={styles.submitbtn}>Continue with Google</button>
+        </div>
+    )
+}
